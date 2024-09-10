9a29fa3f0efe49ac97dfe1b0cc675c057e2a7848
Karim Ahmed <karim2elsaid@gmail.com>
2024-09-10 17:40:52 +0300

    Changess

--- speedoTransfer/AppDelegate.swift
+++ speedoTransfer/AppDelegate.swift
@@ -4,21 +4,24 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
 
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        // Initialize the window
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        
-//        // Load the main storyboard
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        
-//        // Instantiate the initial view controller from the storyboard
-//        let initialViewController = storyboard.instantiateViewController(withIdentifier: "FirstSignUpViewController")
-//        
-//        // Set the root view controller and make the window visible
-//        window?.rootViewController = initialViewController
-//        window?.makeKeyAndVisible()
-//        
-//        return true
-//    }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Initialize the window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        
+        
+        // Load the main storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // Instantiate the initial view controller from the storyboard
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "FirstSignUpViewController")
+        
+        // Set the root view controller and make the window visible
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
 }
