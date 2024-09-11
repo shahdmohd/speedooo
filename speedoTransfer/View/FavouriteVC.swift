6a36d3b576950b8f00e603905478a2fbc1aed386
Shahd Mohammed <shahdmohamed21203@gmail.com>
2024-09-11 16:09:27 +0300

    changes 4:09

--- speedoTransfer/View/FavouriteVC.swift
+++ speedoTransfer/View/FavouriteVC.swift
@@ -30,4 +30,54 @@ static func create() -> FavouriteVC {
                 navigationController?.popViewController(animated: true)
             }
 
+    
+        // Register custom cell
+        moreFavListTableView.register(UINib(nibName: "favListCell", bundle: nil), forCellReuseIdentifier: "favListCell"))
+        
+        moreFavListTableView.delegate = self
+        moreFavListTableView.dataSource = self
+     
+        
+        
+        
+        
+        // Set the background color of the table view to clear
+        moreTableView.backgroundColor = UIColor.clear
+              
+        // Reload the table view to reflect the data
+        moreTableView.reloadData()
+        
+        // Set the gradient background
+        profileVc.secondBackgroundColor(to: self.view)
+}
+
+
+
+extension MoreVC: UITableViewDelegate, UITableViewDataSource {
+// This function is related to delegate
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  
+}
+
+// Both these functions are for Data Source
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tasks.count
+}
+
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "MoreTVCell", for: indexPath) as! MoreTVCell
+    
+    // Configure the cell with the corresponding text from the array
+    let text = tasks[indexPath.row]
+    let icon = icons[indexPath.row]
+    cell.configureCell(media: Media(title: text, image: icon))
+    cell.delegate = self
+    return cell
+}
+}
+    
+    
+    
 }
--- speedoTransfer/untitled folder/favListCellTableViewCell.swift
+++ speedoTransfer/untitled folder/favListCellTableViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class favListCellTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var bankImage: UIImageView!
+    @IBOutlet weak var UserName: UILabel!
+    @IBOutlet weak var accountNum: UILabel!
+    @IBOutlet weak var editIcon: UIImageView!
+    @IBOutlet weak var deleteIcon: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
